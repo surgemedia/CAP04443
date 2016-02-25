680482c1dbf9e7d4897e72670812ecb8441b3997
Alex <alexanderddking@gmail.com>
2016-02-25 14:59:07 +1000

    Customisable Package structure

--- wp-content/themes/Basetheme/components/organism-single-package.php
+++ wp-content/themes/Basetheme/components/organism-single-package.php
@@ -1,26 +1,26 @@
 <?php
 	$id=$args[1];
 	$color=$args[2];
+	$product_meta = get_post_meta($id);
+
 ?>
 <?php 	includePart('components/molecule-package.php',
 										$id,
-										"icon-insects", //$icon
-															"Mini",					//$line1
-												"Value Pack",		//$line2
-										"Class + Mini Portrait Set",//$info
-										"$30", //$price
+										get_field('icon',$id), //$icon
+										explode(" ",get_post($id)->post_title, 2)[0],	//$line1
+										explode(" ",get_post($id)->post_title, 2)[1],	//$line2
+										get_field('subtitle',$id),//$info
+										"$".$product_meta['_price'][0],//$price
 										"full", //length
-										"The Mega Value Pack represents MEGA value, especially for families who like to send a decent sized photo to both sets of grandparents.
-Our class groups are on a 8x10inch template (20.3x25.4cm). They are laminated in 100 micron film (as opposed to the more commonly used 80 micron film), which means they have a premium quality glass-like finish.
-You can choose our standard border (your school colours), one of the custom borders (incorporating photos taken around your school) or we can work with you to develop a unique one to suit your tastes.",
+										get_the_content($id),
 										$color //color
 );?>
 <?php 	includePart('components/molecule-package-info.php',
 											$id, //id
-											"Mini Value Pack", //title
-											"The Mega Value Pack represents MEGA value, especially for families who like to send a decent sized photo to both sets of grandparents.
-	Our class groups are on a 8x10inch template (20.3x25.4cm). They are laminated in 100 micron film (as opposed to the more commonly used 80 micron film), which means they have a premium quality glass-like finish.
-	You can choose our standard border (your school colours), one of the custom borders (incorporating photos taken around your school) or we can work with you to develop a unique one to suit your tastes.",
+											get_post($id)->post_title, //title
+											get_the_content($id),
 											$color,
 											"hidden-xs"
-);?>
+);
+unset($product_meta);
+?>
--- wp-content/themes/Basetheme/template-shop.php
+++ wp-content/themes/Basetheme/template-shop.php
@@ -28,68 +28,90 @@
 <div class="col-lg-6 packages col-lg-push-6 right-side">
     <?php
 
+//debug(get_field('package_under_school', 'school_'.$school->term_id)[0]['products_in_section']);
 // Setup your custom query
+//debug(get_field('package_under_school', 'school_'.$school->term_id));
+
+
+
+
+for ($i=0; $i < sizeof(get_field('package_under_school', 'school_'.$school->term_id)); $i++) { 
+    # code...
+$product_section = get_field('package_under_school', 'school_'.$school->term_id)[$i];
+if($product_section['title']){
+        echo '<span>'.$product_section['title'].'</span>';
+    }
 $args = array( 
     'post_type'              => array( 'product' ),
-    'post__in'              => array(  get_field('products_listing', 'school_'.$school->term_id),),
-     'tax_query' => array(
-        array(
-            'taxonomy' => 'school',
-            'field'    => 'slug',
-            'terms'    => $term_name,
-        ),
-      ),
-    );
-unset($school);
-
-//debug($args);
-$products = Array();
+    'orderby'              => 'post__in',
+    'post__in'              => $product_section['products_in_section'],
+     'tax_query' => array( array(  'taxonomy' => 'school',   'field' => 'slug', 'terms'    => $term_name,  ),  ), );
+//debug($product_section['products_in_section']);
 $loop = new WP_Query( $args );
-$temp = Array();
-$count = 1;
+$two_pack = Array();
 while ( $loop->have_posts() ) : $loop->the_post(); ?>
+
     <?php 
-    $size = get_field('size',get_the_id());
-
-    debug($size);
-    // if($size == ){
-    // array_push($temp,get_post());
-    // $count++;
-    // } else {
-    //      array_push($products,Array(get_post()));
-    //      $count = 1;
-    // } 
-    // if(sizeof($temp) == 2){
-    // array_push($products,$temp);
-    // $temp = Array();
-    // }
-     ?>
-<?php endwhile; wp_reset_query(); // Remember to reset ?>
-
-
-
-<?php //debug($products); ?>
-<?php for ($i=0; $i < sizeof($products); $i++) {
-
-        if(sizeof($products[$i]) == 2){
-
-        includePart('components/organism-double-package.php',
-        $products[$i][0]->ID, //$id1
-        $products[$i][1]->ID, //$id2
-        get_field('color',$products[$i][0]),
-        get_field('color',$products[$i][1]),
-        get_post_meta($products[$i][0]->ID),
-        get_post_meta($products[$i][1]->ID)
-        );
-        }
-
-        if(sizeof($products[$i]) == 1){
+    
+    if(get_field('size',get_post()->ID) == 'full'){
+         //debug(get_post());
         includePart('components/organism-single-package.php',
-        $products[$i][0]->ID, //$id1
-        get_field('color',$products[$i][0])
+        get_post()->ID, //$id1
+        get_field('color',get_post()->ID)
         ); 
+    }
+
+    if(get_field('size',get_post()->ID) == 'half'){
+        array_push($two_pack,get_post());
+        if(sizeof($two_pack) == 2){
+            //debug(get_post());
+             //debug($two_pack);
+                includePart('components/organism-double-package.php',
+                $two_pack[0]->ID, //$id1
+                $two_pack[1]->ID, //$id2
+                get_field('color',$two_pack[0]),
+                get_field('color',$two_pack[1]),
+                get_post_meta($two_pack[0]->ID),
+                get_post_meta($two_pack[1]->ID)
+                );
+                
+             $two_pack = Array();
         }
-}
+    }
+       
+
+
+     ?>
+<?php endwhile; 
+wp_reset_query(); 
+unset($product_section);
+} //end for loop
+//unset($school);
+ ?>
+
+
+
+<?php 
+// for ($i=0; $i < sizeof($products); $i++) {
+
+//         if(sizeof($products[$i]) == 2){
+//         includePart('components/organism-double-package.php',
+//         $products[$i][0]->ID, //$id1
+//         $products[$i][1]->ID, //$id2
+//         get_field('color',$products[$i][0]),
+//         get_field('color',$products[$i][1]),
+//         get_post_meta($products[$i][0]->ID),
+//         get_post_meta($products[$i][1]->ID)
+//         );
+//         }
+
+//         if(sizeof($products[$i]) == 1){
+//         includePart('components/organism-single-package.php',
+//         $products[$i][0]->ID, //$id1
+//         get_field('color',$products[$i][0])
+//         ); 
+//         }
+// }
  ?>
 
 </div>
